0dfdde363566c231cd2851e120adbff2e260dacb
danielemanuelheim <heim.daniel.311@gmail.com>
2023-02-12 16:02:54 +0100

    added card shuffling
    - when the site is loaded, the cards receive a random order
    - the images are inserted on the symbol side of the card

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 
 let karten = document.querySelectorAll(".karte");
+let kartenArray = [...karten];
+
+function kartenMischen() {
+  kartenArray.forEach((karte) => {
+    let randomIndex = Math.floor(Math.random() * kartenArray.length);
+    karte.style.order = randomIndex;
+    karte.children[1].style.backgroundImage = `url(${karte.getAttribute(
+      "data-bild"
+    )})`;
+  });
+}
 
 function karteWenden() {
     this.classList.add("gewendet")
 }
 
 function starten() {
+    kartenMischen();
     karten.forEach((karte) => karte.addEventListener("click", karteWenden));
 }
 
